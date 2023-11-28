7b2d21074cb5cd9835f3183289d3e1deaf0f51c3
Filip <filber43@gmail.com>
2023-11-28 23:50:20 +0100

    changed main.js

--- js/main.js
+++ js/main.js
@@ -1,40 +1,51 @@
-const actualDate = document.querySelector('.actual-date');
+const actualDate = document.querySelector(".actual-date");
 
 const handleCurrentYear = () => {
-  const year = (new Date).getFullYear();
-  actualDate.innerText = year;
-}
+	const year = new Date().getFullYear();
+	actualDate.innerText = year;
+};
 handleCurrentYear();
 
-var options = {
-    series: [{
-    name: 'Aktywni użytkownicy',
-    data: [88, 40, 28, 51, 102, 109, 70]
-  }, {
-    name: 'Wystawiane posty',
-    data: [22, 32, 45, 32, 34, 32, 41]
-  }],
-    chart: {
-    height: 250,
-    type: 'area',
-    foreColor: '#fff'
-  },
-  dataLabels: {
-    enabled: false
-  },
-  stroke: {
-    curve: 'smooth'
-  },
-  xaxis: {
-    type: 'datetime',
-    categories: ["2023-08-19T00:00:00.000Z", "2023-08-19T01:30:00.000Z", "2023-08-19T02:30:00.000Z", "2023-08-19T03:30:00.000Z", "2023-08-19T04:30:00.000Z", "2023-08-19T05:30:00.000Z", "2023-08-19T06:30:00.000Z"]
-  },
-  tooltip: {
-    x: {
-      format: 'dd/MM/yy HH:mm'
-    },
-  },
-  };
+let options = {
+	series: [
+		{
+			name: "Aktywni użytkownicy",
+			data: [88, 40, 28, 51, 102, 109, 70],
+		},
+		{
+			name: "Wystawiane posty",
+			data: [22, 32, 45, 32, 34, 32, 41],
+		},
+	],
+	chart: {
+		height: 250,
+		type: "area",
+		foreColor: "#fff",
+	},
+	dataLabels: {
+		enabled: false,
+	},
+	stroke: {
+		curve: "smooth",
+	},
+	xaxis: {
+		type: "datetime",
+		categories: [
+			"2023-08-19T00:00:00.000Z",
+			"2023-08-19T01:30:00.000Z",
+			"2023-08-19T02:30:00.000Z",
+			"2023-08-19T03:30:00.000Z",
+			"2023-08-19T04:30:00.000Z",
+			"2023-08-19T05:30:00.000Z",
+			"2023-08-19T06:30:00.000Z",
+		],
+	},
+	tooltip: {
+		x: {
+			format: "dd/MM/yy HH:mm",
+		},
+	},
+};
 
-  var chart = new ApexCharts(document.querySelector("#chart"), options);
-  chart.render();
+let chart = new ApexCharts(document.querySelector("#chart"), options);
+chart.render();
